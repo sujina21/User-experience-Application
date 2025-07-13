03ae948b244a385de39800fd82de9b1099bf7ff2
Sujina21 <sujinashrestha060@gmail.com>
2025-07-13 22:48:20 +0545

    Add or update account_view.dart

---
+++ lib/features/home/presentation/view/bottom_view/account_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountView extends StatelessWidget {
+  const AccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Account View',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
