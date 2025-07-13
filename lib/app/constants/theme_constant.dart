e6ba7c6f0c638dfc5a78b7c089fd5858c13b8bf1
Sujina21 <sujinashrestha060@gmail.com>
2025-07-13 22:36:40 +0545

    Add or update theme_constant.dart

---
+++ lib/app/constants/theme_constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeConstant {
+  ThemeConstant._();
+
+  static const Color darkPrimaryColor = Color(0xFFEF93B2);
+  static const Color primaryColor = Colors.green;
+  static const Color appBarColor = Colors.green;
+}
