360ede3049b880cbe753d31000970a003d599c4b
Sujina21 <sujinashrestha060@gmail.com>
2025-07-13 22:48:20 +0545

    Add or update course_view.dart

---
+++ lib/features/home/presentation/view/bottom_view/course_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CourseView extends StatelessWidget {
+  const CourseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Course View',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
